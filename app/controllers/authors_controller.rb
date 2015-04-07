075dfd971eff655ec713117c6c2c6cc59103b1ee
Steven Huang <szteven3@gmail.com>
2015-04-07 23:01:17 +0800

    finish author#new create and index

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,40 @@
 class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+  end
+
+  def show
+    @author
+  end
+
+  def new
+    @page_title = 'Add New Author'
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to authors_path
+      flash[:notice] = 'Author Created!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name)
+  end
 end
